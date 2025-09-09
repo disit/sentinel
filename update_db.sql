608ee6ae59bcb3d1db528b00a8fdc71cf6f28031
ElMustacho <fabri.mereu@gmail.com>
2025-09-09 16:26:49 +0200

    Minor fixes

--- update_db.sql
+++ update_db.sql
@@ -4,8 +4,8 @@ ALTER TABLE `checker`.`cronjob_history`
 CHANGE COLUMN `result` `result` MEDIUMTEXT NOT NULL ,
 CHANGE COLUMN `errors` `errors` MEDIUMTEXT NULL DEFAULT NULL ;
 
--- replace namespace with namespace of microx                                                                                                         here
---                                                                                                                                                     |
---                                                                                                                                                    \ /
---                                                                                                                                                     V
-INSERT INTO `checker`.`cronjobs` (`idcronjobs`, `name`, `command`, `category`) VALUES (NULL, 'Crawler', 'kubectl exec deployment/dashboard-cron -n dashk8stest -- bash -s < scripts/read_crawler_logs.sh', '1');
+-- replace namespace with namespace of microx                                                                                                           here
+--                                                                                                                                                       |
+--                                                                                                                                                      \ /
+--                                                                                                                                                       V
+INSERT INTO `checker`.`cronjobs` (`idcronjobs`, `name`, `command`, `category`) VALUES (NULL, 'Crawler', 'kubectl exec -i deployment/dashboard-cron -n dashk8stest -- bash -s < scripts/read_crawler_logs.sh', '1');
